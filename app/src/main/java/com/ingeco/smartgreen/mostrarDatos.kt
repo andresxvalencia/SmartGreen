8d94a20f0055dfc2437e1645ba5684ead3828103
juanegallegob <juane.gallegob@uqvirtual.edu.co>
2021-12-06 23:28:17 -0500

    Multiples Cambios

--- app/src/main/java/com/ingeco/smartgreen/mostrarDatos.kt
+++ app/src/main/java/com/ingeco/smartgreen/mostrarDatos.kt
@@ -1,38 +1,30 @@
 package com.ingeco.smartgreen
 
-import android.Manifest
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothServerSocket
 import android.bluetooth.BluetoothSocket
-import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
-import android.widget.Button
-import android.widget.TextView
+import android.os.Message
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.common.util.Strings
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-import org.json.JSONArray
-import java.io.IOException
-import java.io.InputStream
-import java.text.DateFormat
 import java.text.DateFormat.*
-import java.text.SimpleDateFormat
 import java.util.*
 import java.util.UUID.randomUUID
-import org.json.JSONObject
-import org.json.JSONException
-import java.lang.reflect.Array.get
 
-
-
-
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.TextView
+import android.widget.Toast
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+import java.lang.StringBuilder
+import android.system.Os.socket
 
 
 
@@ -49,6 +41,7 @@ class mostrarDatos : AppCompatActivity() {
         btnCerrarSesion = findViewById(R.id.btnCerrarSesion)
         btnNuevoValor = findViewById(R.id.btnNuevoValor)
         btnVincular = findViewById(R.id.btnVincular)
+
 
         setup()
         visualizarDatos()
@@ -123,19 +116,20 @@ class mostrarDatos : AppCompatActivity() {
             val database = Firebase.database
             val dateValue = database.getReference(setDate)
 
-            data class Data( val TDS: String? = null, val pH: String?) {
+            data class Data( val data: String? ) {
                 // Null default values create a no-argument default constructor, which is needed
                 // for deserialization from a DataSnapshot.
             }
 
-            val TDS: String = "1300"
-            val pH: String = "7"
+            val data: String = "pH: 7 TDS: 1300"
 
-            var Texto : String = "Hora: " + time + "        pH: " + pH + "      TDS: " + TDS
+            var Texto : String = data
 
-            val newData = Data(TDS,pH)
+            val newData = Data(data)
 
             dateValue.setValue(newData)
+
+            /**
 
             val TextDate = findViewById(R.id.Fecha) as TextView
             TextDate.setText("Fecha donde se tomaron los datos: " + date);
@@ -145,9 +139,27 @@ class mostrarDatos : AppCompatActivity() {
             TextData.setText(Texto);
             TextData.setTextColor(Color.BLUE);
             TextData.setTextSize(20F);
-
-
             Texto = Texto + "\n"
+
+            */
+
+            val arrayAdapter: ArrayAdapter<*>
+            var list = arrayOf(
+                "Datos tomados en: " + date
+            )
+
+            list += Texto
+
+
+            val listView = findViewById<ListView>(R.id.listDevices)
+            arrayAdapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_list_item_1, list
+            )
+
+            listView.adapter = arrayAdapter
+
+
 
         }
 
@@ -156,28 +168,52 @@ class mostrarDatos : AppCompatActivity() {
     private fun recibirDatosBT(){
 
         btnVincular.setOnClickListener{
+
+
+            val arrayAdapter: ArrayAdapter<*>
+            var list = arrayOf(
+                "Dispositivos Vinculados"
+            )
+
             val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
             if (bluetoothAdapter == null) {
                 // Device doesn't support Bluetooth
             }
             val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
             pairedDevices?.forEach { device ->
-                val deviceName = device.name
-                val deviceHardwareAddress = device.address // MAC address
+                var deviceName = device.name
+                var deviceHardwareAddress = device.address // MAC address
 
-                val name = findViewById(R.id.deviceName) as TextView
-                name.setText(deviceName);
-                name.setTextColor(Color.RED);
+                val labeltxt = deviceName + ": " + deviceHardwareAddress
 
-                val address = findViewById(R.id.deviceAddress) as TextView
-                address.setText(deviceHardwareAddress);
-                address.setTextColor(Color.RED);
-
-                val mmServerSocket: BluetoothServerSocket? by lazy(LazyThreadSafetyMode.NONE) {
-                    bluetoothAdapter?.listenUsingInsecureRfcommWithServiceRecord("BT", randomUUID ())
-                }
-
+                list += labeltxt
             }
+
+            val listView = findViewById<ListView>(R.id.listDevices)
+            arrayAdapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_list_item_1, list
+            )
+
+            listView.adapter = arrayAdapter
+
+            listView.setOnItemClickListener(OnItemClickListener { parent, view, position, id -> // selected item
+
+                val label = findViewById(R.id.device) as TextView
+
+                val selectedItemText = parent.getItemAtPosition(position)
+
+                label.setText("Dispositivo Seleccionado :\n $selectedItemText")
+
+                val info : String = label.getText().toString()
+
+                val l = info.length
+
+                val address : String = info.substring(l - 17);
+
+            })
+
+
         }
     }
 }
